14d128d6822dd79f6a54fe6ce1ee3c5d7811a18e
TomMarazzo <camenm@hotmail.com>
2020-11-12 12:36:04 -0500

    update of Tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -12,21 +12,22 @@ router.get('/', (req, res, next) => {
     //use the task model to fetch a list of tasks and pass these to the view display
     //if err, the err parameter will be filled
     //if not, the task parameter will be filled with the query result
-    Task.find((err,tasks)=>
-    {
-        if (err)
-        {
+    Task.find((err, tasks) => {
+        if (err) {
             console.log(err)
             res.end(err)
         }
-        else
-        {
-            res.render('tasks/index', {
-                tasks: tasks
-            })
+        else {
+            res.render('tasks/index',
+                {
+                    tasks: tasks
+                })
         }
     })
-
+})
+//GET tasks add view
+router.get('/add', (req, res, next) => {
+    res.render('tasks/add')
 })
 
 //Make Controller Public
