9bc6d12fda67a4aa437899c53994f1bf6bf6ffb6
TomMarazzo <camenm@hotmail.com>
2020-11-20 08:51:00 -0500

    Added facebook Login

--- config/globals.js
+++ config/globals.js
@@ -7,6 +7,12 @@ module.exports =
             clientSecret: 'ICCm6F2Si3MVd_OVKo-8HSqi',
             callbackURL: 'http://localhost:3000/google/callback'
             //callbackURL: 'https://task-manager-the2nd.herokuapp.com/google/callback'
-        }
+        },
+            'facebook': {
+                clientID: '284983652946610',
+                clientSecret: '3dff81e1932f00fe9d6cb0f5e22d89ca',
+                callbackURL: 'http://localhost:3000/facebook/callback'
+                //callbackURL: 'https://task-manager-the2nd.herokuapp.com/facebook/callback'
+            }
         }
 }
